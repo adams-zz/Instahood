b96c723b6517090e3ccf4808fb51ad306e362a2a
Mike Adams <michaelglenadams@gmail.com>
2013-04-04 10:28:27 -0700

    Commenting code

--- client/instahood.js
+++ client/instahood.js
@@ -6,23 +6,26 @@ Meteor.startup(function(){
   Session.set('photoset', '');
   Session.set('zoomed', ''); 
   getTwitter();
+
+  //Get the users geolocation
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
   } else {
-      alert('It appears that Geolocation, which is required for this web page application, is not enabled in your browser. Please use a browser which supports the Geolocation API.');
+    alert('It appears that Geolocation, which is required for this web page application, is not enabled in your browser. Please use a browser which supports the Geolocation API.');
   }
 
+  //If the user has a geolocation, get photos near location and set map center to location.
   function successFunction(success) {
-      var navLatLng = newLatLng(success);
-      getNewPhotos({lat: success.coords.latitude, lng: success.coords.longitude, distance:'3000', client_id: INSTAID});
-      createMap(navLatLng);
-      placeNavMarker(navLatLng);
-      addClickListener();
-      addAutocomplete();
+    var navLatLng = newLatLng(success);
+    getNewPhotos({lat: success.coords.latitude, lng: success.coords.longitude, distance:'3000', client_id: INSTAID});
+    createMap(navLatLng);
+    placeNavMarker(navLatLng);
+    addClickListener();
+    addAutocomplete();
   }
 
+  //If the user did not enable geolocation, get photos and set map near Golden Gate Bridge.
   function errorFunction(success) {
-    // alert("You've disabled your geolocation... So here are some pretty pictures of the Golden Gate bridge... You can always click around on the map or use the search to see more photos");
     var latlng = new google.maps.LatLng(37.808631, -122.474470);
     getNewPhotos({lat: latlng.lat(), lng: latlng.lng(), distance:'3000', client_id: INSTAID});
     createMap(latlng);
@@ -31,22 +34,18 @@ Meteor.startup(function(){
     addAutocomplete();
   }
 
+  //Hack Reactor Banner
   $('body').append('<a href="http://hackreactor.com"><img style="position: absolute; top: 0; right: 0; border: 0;" src="http://i.imgur.com/x86kKmF.png alt="Built at Hack Reactor"></a>');
   $('#zoomed-image').hide();
 });
 
+
+//Helper function to push photos to template scope
 Template.instagram.photoset = function(){
   return Session.get('photoset');
 }
 
-// Template.instagram.time = function () {
-//   return moment(this.created_time).fromNow();
-// }
-
-//Create Template for Zoom View
-//Use events to trigger Session state of zoom
-//Create Helper Events to handle logic of zoom photo
-
+//Event Handlers for click and mouseover events
 Template.content.events({
   'click .photo': function(event){
     $('.photo').addClass('greyed');
@@ -93,10 +92,12 @@ Template.content.events({
 
 //GOOGLE MAPS HELPERS
 
-function newLatLng(success) {
+//Converts HTML5 geolocation data into a google maps location object
+var newLatLng = function (success) {
    return new google.maps.LatLng(success.coords.latitude, success.coords.longitude);
 }
 
+//Takes a google maps location object (latLng), creates the map and centers it at latLng.
 function createMap(latLng) {
   var mapOptions = {
     streetViewControl: false,
@@ -108,6 +109,8 @@ function createMap(latLng) {
   map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 }
 
+/* The click listener watches for clicks on the map, extracts the Lat & Lng and calls
+   getNewPhotos with the new latLng object. */
 function addClickListener() {
   google.maps.event.addListener(map, 'click', function(event){
     var currentPos = {lat: event.latLng.lat(), lng: event.latLng.lng(), dist: '1000'};
@@ -117,6 +120,9 @@ function addClickListener() {
   });
 }
 
+/* addAutocomplete uses the Google Maps API to create a search field, process the input to get
+   the Lat & Lng for the selected location.  It passes the new latLng to getNewPhotos and 
+   set map center and place new marker */
 function addAutocomplete() {
   var input = document.getElementById('searchTextField');
   autocomplete = new google.maps.places.Autocomplete(input);
@@ -130,6 +136,7 @@ function addAutocomplete() {
   });
 }
 
+//Nav marker is placed only once, at the users location (if it is avaliable)
 function placeNavMarker(latLng) {
   var image = "http://gmaps-samples.googlecode.com/svn/trunk/markers/blue/blank.png";
   var blueIcon = new google.maps.Marker({
@@ -138,9 +145,11 @@ function placeNavMarker(latLng) {
       icon: image
   });
 }
+
+//Instagram markers are placed for every photo, using the Lat & Lng information for the photo
 function placeInstaMarkers(data, map) {
   for (var i = 0; i < data.length; i++) {
-    var latLng = new google.maps.LatLng(data[i].location.latitude, data[i].location.longitude); 
+    var latLng = new google.maps.LatLng(data[i].location.latitude, data[i].location.longitude);
     var image = '/instagram-shadow.png';
     var instaMarker = new google.maps.Marker({
         position: latLng,
@@ -152,18 +161,19 @@ function placeInstaMarkers(data, map) {
   };
 }
 
+//Google Maps API has built in InfoWindows.  This funciton creates new InfoWindows for each photo.
 function addInfoWindow(data, instaMarker, i){
   var username = data[i].user.username;
   var caption;
   if ( !data[i].caption ) {
     caption = "No Comment.."
   } else {
-    caption = data[i].caption.text 
+    caption = data[i].caption.text;
   }
   var infowindow = new google.maps.InfoWindow({
     // backgroundColor: 'rgb(57,57,57)',
     // backgroundClassName: 'phoney',
-    content: 
+    content:
     '<img class="popupPhoto" src="'+ data[i].images.standard_resolution.url +'"/><br/>'+
     '<div class="userInfo">'+
       '<a href="http://instagram.com/'+ username +'" target="_blank">'+
@@ -183,6 +193,8 @@ function addInfoWindow(data, instaMarker, i){
   });
 }
 
+// Ensures only one Instagram marker is open on the map at a time.
+
 function deleteInstaMarkers() {
   if (instaArray) {
     for (i in instaArray){
@@ -192,6 +204,8 @@ function deleteInstaMarkers() {
   instaArray.length = 0;
 }
 
+// Places a single red marker when the user clicks anywhere on the map.
+
 function placeClickMarker(location) {
   deleteOverlays();
   var marker = new google.maps.Marker({
@@ -200,6 +214,8 @@ function placeClickMarker(location) {
   });
   markersArray.push(marker);
 }
+
+// Ensures only one click marker is on the screen at a time, called by placeClickMarker
 
 function deleteOverlays() {
   if (markersArray) {
@@ -212,6 +228,7 @@ function deleteOverlays() {
 
 //INSTA HELPERS
 
+//processses the json data on ajax success
 function jsonLoad (json) {
   if (json.meta.code == 200) {
     var show = json.data;
@@ -224,6 +241,7 @@ function jsonLoad (json) {
   };
 }
 
+//basic ajax call to instagram API, searching for photos within specified distance of passed in place
 var getNewPhotos = function (place) {
   $.ajax({
     url: 'https://api.instagram.com/v1/media/search?callback=?',
@@ -239,6 +257,8 @@ var getNewPhotos = function (place) {
 };
 
 //GENERAL HELPERS
+
+//required by the tweet button in the top right of the nav bar
 var getTwitter = function() {
   !function(d,s,id){
     var js,fjs=d.getElementsByTagName(s)[0];
@@ -250,4 +270,11 @@ var getTwitter = function() {
   }(document,"script","twitter-wjs");
 }
 
-$('div#social').hide()
+// Template.instagram.time = function () {
+//   return moment(this.created_time).fromNow();
+// }
+
+// TO-DO:
+//Create Template for Zoom View
+//Use events to trigger Session state of zoom
+//Create Helper Events to handle logic of zoom photo
